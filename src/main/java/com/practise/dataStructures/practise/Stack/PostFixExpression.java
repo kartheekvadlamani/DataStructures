59fb0356c7fface0f4c1996d3cb0ffabf6bcf5eb
Kartheek Vadlamani <kartheekvadlamani@Kartheeks-MacBook-Pro.local>
2020-05-19 21:43:08 -0500

    solved postfix expression

--- src/main/java/com/practise/dataStructures/practise/Stack/PostFixExpression.java
+++ src/main/java/com/practise/dataStructures/practise/Stack/PostFixExpression.java
@@ -1,4 +1,41 @@
 package com.practise.dataStructures.practise.Stack;
 
+import java.util.Stack;
+
 public class PostFixExpression {
+
+    public static int evaluatePostFixExpression(String expression) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char character = expression.charAt(i);
+
+            if (!Character.isDigit(character)) {
+                Integer x = stack.pop();
+                Integer y = stack.pop();
+
+                switch (character) {
+                    case '+':
+                        stack.push(y + x);
+                        break;
+                    case '-':
+                        stack.push(y - x);
+                        break;
+                    case '*':
+                        stack.push(y * x);
+                        break;
+                    case '/':
+                        stack.push(y / x);
+                        break;
+                }
+
+            } else
+                stack.push(Character.getNumericValue(character));
+        }
+        return stack.pop();
+    }
+    public static void main(String args[]) {
+
+        System.out.println(evaluatePostFixExpression("921*-8-4+"));
+
+    }
 }
